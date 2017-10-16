541392ebfdbdb180245b5aa046f1f6414499c8e3
Vladyslav Nesterenko <easterenko@gmail.com>
2017-10-16 17:21:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 function measureWater(rocks) {
-  console.log(
-    rocks.reduce(
-      (a,b, i ,arr) => {
-        if (i > 1) {
-          
-        }
-        if (i > arr.length-3) {
-          alert(arr[i]);
-        }
-      },
-      0
-    )
-  );
+  var len = rocks.length;
+  var water = 0;
   
+  for (var i = 2; i < len; i++) {
+    if (rocks[i - 2] > rocks[i - 1] && rocks[i] > rocks[i - 1]) {
+      if (rocks[i] > rocks[i - 2])
+        water += rocks[i] - rocks[i - 2] + rocks[i - 1];
+    }
+  }
 }
 
 // Tests
 
 var inputs = [
-  [2, 5, 1, 3, 1, 2, 1, 7, 7, 6],/*
+  [2, 5, 1, 3, 1, 2, 1, 7, 7, 6],
   [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0],
   [7, 0, 1, 3, 4, 1, 2, 1],
   [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0],
   [2, 2, 1, 2, 2, 3, 0, 1, 2],
   [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 8],
-  [2, 2, 2, 2, 2]*/
+  [2, 2, 2, 2, 2]
 ];
 
 var outputs = [17,10,9,10,4,24,0]
