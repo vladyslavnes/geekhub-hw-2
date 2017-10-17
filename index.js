4a316efef3fd7c9ce2fb1aa461b0052ebf7a71a3
Vladyslav Nesterenko <easterenko@gmail.com>
2017-10-17 20:19:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-function measureWater(rocks) {
-  var len = rocks.length;
-  var water = 0;
-  
-  for (var i = 2; i < len; i++) {
-    if (rocks[i - 2] > rocks[i - 1] && rocks[i] > rocks[i - 1]) {
-      if (rocks[i] > rocks[i - 2])
-        water += rocks[i] - rocks[i - 2] + rocks[i - 1];
-    }
-  }
-}
+function measureWater(arr) {
+	var water = 0;
+	var cursor = 0;
 
+	arr.reduce((prev, next, i) => {
+		if(prev <= next) {
+			cursor = i;
+			return next;
+		} else {
+			water += prev - next;
+			return prev;
+		}
+	}, 0);
+  
+	var newArr = arr.splice(cursor);
+  
+	newArr.reduceRight((right, next) => {
+		if(next > right) {
+			right = next;
+		}
+    
+		water -= newArr[0] - right;
+		return right;
+	}, 0); 
+  
+	return water;
+}
 // Tests
 
 var inputs = [
@@ -24,9 +39,11 @@ var inputs = [
 
 var outputs = [17,10,9,10,4,24,0]
 
-for (var i in inputs)
+for (var i in inputs) {
+  console.log(measureWater(inputs[i]));
   if (measureWater(inputs[i]) === outputs[i])
     console.log(`Test ${i} passed`)
   else
     console.log(`Test ${i} not passed`)
+}
     
